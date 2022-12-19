bc317537212024face4d77102434591a25c3f976
peanutBana <diao0321@gmail.com>
2022-12-19 11:43:08 +0900

    'return_students'

--- src/main/java/ch09/StudentController.java
+++ src/main/java/ch09/StudentController.java
@@ -30,16 +30,31 @@ public class StudentController extends HttpServlet {
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("utf-8");
 		//request : 뷰에서 넘어온 데이터의 정보 저장되어있음
-		String action = request.getParameter("action");
+		String action = request.getParameter("action");		//
 		String view="";
 		
-		view = insert(request, response);	//request와 response객체를 매개변수로 넘겨준다. ==>
-		
-		//getRequestDispatcher: 이동할 페이지의 경로 지정
-		//forward: 페이지를 이동시킴. 내부에서 이동이 되므로 주소가 변하지 않는다.
-		getServletContext().getRequestDispatcher("/ch09/" + view).forward(request, response);
+		if(action == null) {
+			//리퀘스트를 또 한다.
+			getServletContext().getRequestDispatcher("/studentControl?action=list").forward(request, response);
+		}
+		else {
+			switch(action) {
+				case "list": view = list(request, response); break;
+				case "insert": view = insert(request, response);  break;	//request와 response객체를 매개변수로 넘겨준다. ==>
+			}
+			//getRequestDispatcher: 이동할 페이지의 경로 지정
+			//forward: 페이지를 이동시킴. 내부에서 이동이 되므로 주소가 변하지 않는다.
+			getServletContext().getRequestDispatcher("/ch09/" + view).forward(request, response);
+		}
+	}
+	
+	public String list(HttpServletRequest request, HttpServletResponse response) {
+		//request.setAttribute(key, value)
+		request.setAttribute("students", dao.getAll());		//request, response 할 때 값을 넘겨준다.
+		return "studentInfo.jsp";
 		
 	}
+	
 	
 	//request데이터 받아옴 -> DAO에 있는 insert 실행(DB에 insert됨) -> 페이지명(studentInfo.jsp)에 리턴 
 	public String insert(HttpServletRequest request, HttpServletResponse response) {		
@@ -51,17 +66,9 @@ public class StudentController extends HttpServlet {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} 
-		
-		
 		dao.insert(s);		//컨트롤러는 DAO에 있는 메소드를 사용한다. / DAO한테 요청해야한다.
 		
 		return "studentInfo.jsp";
-		
 	}
-	
-
-	
-       
-	
 
 }
--- src/main/webapp/ch09/studentInfo.jsp
+++ src/main/webapp/ch09/studentInfo.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,7 +8,27 @@
 <title>Insert title here</title>
 </head>
 <body>
-	<h2>학생 정보</h2>
+	<h2>학생 정보</h2>[<a href="/studentInfo2/studentControl">새로고침</a>]
+	<hr>
+	<table border="1">
+		<tr>
+			<th>id</th>
+			<th>이름</th>
+			<th>대학</th>
+			<th>생일</th>
+			<th>이메일</th>
+		</tr>
+		<c:forEach items="${students}" var="s">
+		<tr>
+			<td>${s.id}</td>
+			<td>${s.username}</td>
+			<td>${s.univ}</td>
+			<td>${s.birth}</td>
+			<td>${s.email}</td>
+		</tr>
+		</c:forEach>
+		
+	</table>
 	
 	<h2>학생 추가</h2>
 	<hr>
